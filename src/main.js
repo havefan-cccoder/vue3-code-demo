b9d904983cce820cfa8c43de2a77da8b0d57a4d8
Maggie <1802941855@qq.com>
2024-04-03 14:38:08 +0800

    添加自定义全局指令

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 自定义指令函数
+const customDirective = {
+    // 当被绑定的元素挂载到 DOM 上时调用
+    mounted(el, binding) {
+      el.addEventListener('click', () => {
+        alert(binding.value.message);
+      });
+    }
+};
+
+const app = createApp(App);
+app.directive('custom', customDirective)
+app.mount('#app')
